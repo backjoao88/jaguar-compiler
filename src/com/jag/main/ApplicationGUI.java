78e31ee8e72eaa9b36bfbfedd77637d244c3a453
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2019-04-14 17:34:02 -0300

    [ADD] Adicionando e criando a implementação inicial do JFrame ScreenMain

---
+++ src/com/jag/main/ApplicationGUI.java
@@ -0,0 +1,12 @@
+package com.jag.main;
+
+import com.jag.gui.ScreenMain;
+
+public class ApplicationGUI {
+    
+    public static void main(String[] args) {
+        ScreenMain screenMain = new ScreenMain();
+        screenMain.setLocationRelativeTo(null);
+        screenMain.setVisible(true);
+    }
+}
